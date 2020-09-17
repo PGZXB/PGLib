eed620af0070133111544fa9a630413c9fadda38
PGZXB <pgzxb@qq.com>
2020-09-17 22:50:29 +0800

    DEBUG

--- src/main.cpp
+++ src/main.cpp
@@ -19,28 +19,32 @@
 
 
 int main() {
-    pg::HttpAppServer s(8080);
+    // pg::HttpAppServer s(8080);
 
-    auto apps = pg::Configure::getInstance()->getFileReponseApplications();
-    s.addApplicationBatch(apps);
+    // auto apps = pg::Configure::getInstance()->getFileReponseApplications();
+    // s.addApplicationBatch(apps);
 
-    for (auto & e : apps) {
-        auto ee = dynamic_cast<pg::HttpFileResponseApplication* >(e);
-        printf("%s, %s, %s\n", ee->url.c_str(), ee->filename.c_str(), ee->contentType.c_str());
-    }
+    // for (auto & e : apps) {
+    //     auto ee = dynamic_cast<pg::HttpFileResponseApplication* >(e);
+    //     printf("%s, %s, %s\n", ee->url.c_str(), ee->filename.c_str(), ee->contentType.c_str());
+    // }
     
-    pg::HttpApplication * app = pg::Configure::getInstance()->getFileReponseApplications().front();
-    pg::AutoHttpApplication autoApp2(
-        [app] (const pg::HttpRequest & request, pg::HttpResponse & response)
-            { app->exec(request, response); }, "/test");
-    pg::AutoHttpApplication autoApp("/index");
-    autoApp.pushChildApplication(&autoApp2);
-    autoApp2.pushChildApplication(
-        pg::Configure::getInstance()->getFileReponseApplications().front()
-    );
-    s.addApplication(&autoApp);
+    // pg::HttpApplication * app = pg::Configure::getInstance()->getFileReponseApplications().front();
+    // pg::AutoHttpApplication autoApp2(
+    //     [app] (const pg::HttpRequest & request, pg::HttpResponse & response)
+    //         { app->exec(request, response); }, "/test");
+    // pg::AutoHttpApplication autoApp("/index");
+    // autoApp.pushChildApplication(&autoApp2);
+    // autoApp2.pushChildApplication(
+    //     pg::Configure::getInstance()->getFileReponseApplications().front()
+    // );
+    // s.addApplication(&autoApp);
 
-    s.run();
+    // s.run();
+
+    for (auto & e : pg::util::stringUtil::__IN_fmtUtil::parseArgs({"1", "2", "3"}, 1, 1, 1)) {
+        printf("%s, ", e.c_str());
+    } printf("\n");
 
     return 0;
 }
