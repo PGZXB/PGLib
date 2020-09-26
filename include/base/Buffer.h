6cf3652cabe7c7e40a32d39e07fb866d7f808418
PGZXB <pgzxb@qq.com>
2020-09-26 13:45:38 +0800

    UPDATE DynamicBuffer; ADD StaticBuffer

--- include/base/Buffer.h
+++ include/base/Buffer.h
@@ -2,8 +2,10 @@
 #define __BUFFER_H__
 
 #include <vector>
+#include <array>
 #include <algorithm>
 #include <cstring>
+#include <stdio.h>
 
 #include <PGDef.h>  // type::Byte
 #include <DynamicArrayWrapper.h>
@@ -11,40 +13,90 @@
 namespace pg::base {
     class DynamicBuffer;
 
+    template <size_t size>
+    class StaticBuffer;
+
     using Buffer = DynamicBuffer;
 
+    // DynamicBuffer
     class DynamicBuffer {
         static const int DEFAULT_SIZE = 4096;
         using Byte = type::Byte;
         using ByteArray = base::DynamicArrayWrapper<Byte>;
     public:
-        DynamicBuffer() : bBuf(DEFAULT_SIZE), cap(DEFAULT_SIZE) { }
+        DynamicBuffer(size_t deftSize = DEFAULT_SIZE) : bBuf_(deftSize), cap_(deftSize) { }
 
-        const Byte * get() const {
-            return bBuf.get();
-        }
+        const Byte * get() const { return bBuf_.get(); }
+
+        std::size_t size() const { return this->size_; }
 
         void clear() {
-            std::memset(bBuf.get(), 0, cap);
+            std::memset(bBuf_.get(), 0, cap_);
+            this->size_ = 0;
         }
 
-        template<typename _Type>
-        void push_back(_Type val) {
-            size_t len = sizeof(_Type);
-            size_t minLen = size + len;
-            if (minLen > cap) {
-                ByteArray tmp(cap = minLen << 1);
-                swap(bBuf, tmp);
-                std::memcpy(bBuf.get(), tmp.get(), size);
-            }
-            std::memcpy(bBuf.get() + size, (void *)&val, len);
-            size = minLen;
+        template <typename _Type>
+        void push_back(const _Type & val) {
+            this->push_back(&val, sizeof(_Type));
+        }
+
+        void push_back(const void * pVal, size_t siz) {
+
+            size_t minLen = siz + this->size_;
+            extend(minLen);
+            std::memcpy(bBuf_.get() + this->size_, (void *)pVal, siz);
+            this->size_ = minLen;
         }
 
     private:
-        ByteArray bBuf;
-        std::size_t size = 0;
-        std::size_t cap = 0;
+        void extend(size_t minLen) {
+            if (minLen <= cap_) return;
+
+            ByteArray tmp(cap_ = minLen << 1);
+            swap(bBuf_, tmp);
+            std::memcpy(bBuf_.get(), tmp.get(), size_);
+        }
+        
+    private:
+        ByteArray bBuf_;
+        std::size_t size_ = 0;
+        std::size_t cap_ = 0;
+    };
+
+    // StaticBuffer
+    template <std::size_t SIZE>
+    class StaticBuffer {
+        using Byte = type::Byte;
+        using ByteArray = Byte[SIZE];
+    public:
+        StaticBuffer() = default;
+
+        const Byte * get() const { return bBuf_; }
+        std::size_t size() const { return this->size_; }
+
+        void clear() {
+            std::memset(bBuf_.get(), 0, SIZE);
+            this->size_ = 0;
+        }
+
+        template <typename _Type>
+        bool push_back(const _Type & val) {
+            return this->push_back(&val, sizeof(_Type));
+        }
+
+        bool push_back(const void * pVal, size_t siz) {
+
+            size_t minLen = siz + this->size_;
+            if (minLen > SIZE) return false;
+            std::memcpy(bBuf_ + this->size_, (void *)pVal, siz);
+            this->size_ = minLen;
+
+            return true;
+        }
+
+    private:
+        ByteArray bBuf_ = { 0 };
+        std::size_t size_ = 0;
     };
 } // namespace pg::base
 
--- test/test_Buffer.cpp
+++ test/test_Buffer.cpp
@@ -5,7 +5,7 @@
 int main () {
     pg::base::Buffer buffer;
 
-    struct String { char str[BUFSIZ] = "aiaiaiaiaaiaiaiaiaiaiaiaiaiaiia"; };
+    struct String { char str[BUFSIZ] = "Test For String"; };
 
     String str;
     int val = 100;
@@ -17,7 +17,22 @@ int main () {
     int val2 = -1;
     std::memcpy(&val2, (void *)(buffer.get()), sizeof(int));
 
-    std::printf("%s\n", buffer.get());
+    for (size_t i = 0; i < buffer.size(); ++i) {
+        putc(*(buffer.get() + i), stdout);
+    } putc('\n', stdout);
+    std::printf("%d\n", *(int *)buffer.get());
+    std::printf("%s\n", buffer.get() + sizeof(int));
+
+    std::printf("----------------------------------------------------------------\n");
+
+    pg::base::StaticBuffer<1024> buffer2;
+
+    buffer2.push_back(val);
+    buffer2.push_back(&str, sizeof(str));
+
+    for (size_t i = 0; i < buffer.size(); ++i) {
+        putc(*(buffer.get() + i), stdout);
+    } putc('\n', stdout);
     std::printf("%d\n", *(int *)buffer.get());
     std::printf("%s\n", buffer.get() + sizeof(int));
 
