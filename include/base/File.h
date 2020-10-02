967f56370e2bbfe37de87112dff6b73397d32ff6
PGZXB <pgzxb@qq.com>
2020-10-02 19:11:55 +0800

    ADD/DEFINE 'File' but not finished;

--- include/base/File.h
+++ include/base/File.h
@@ -6,6 +6,9 @@
 
 #include <memory>
 #include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #include <base/pgfwd.h>
 #include <base/FDWrapper.h>
@@ -14,11 +17,12 @@
 namespace pg::base {
 
     class File {
-        union FileHandle {
-            FILE * file_handle;
-            DIR * dir_handle;
-        };
-        using Handle = FileHandle;
+        // union FileHandle {
+            // FILE * file_handle;
+            // DIR * dir_handle;
+        // };
+        // using Handle = FileHandle;
+        static const char PATH_SEPARATOR;
     public:
         // Java-Like-Design
 
@@ -28,34 +32,34 @@ namespace pg::base {
         File(const std::string & parent, const std::string & child); // 根据 parent 路径名字符串和 child 路径名字符串创建一个新 File 实例。
 
         // member-functions
-        bool readable(); // 测试应用程序是否能从指定的文件中进行读取
+        bool readable() const; // 测试应用程序是否能从指定的文件中进行读取
 
-        bool writable(); // 测试应用程序是否能写当前文件
+        bool writable() const; // 测试应用程序是否能写当前文件
 
         bool remove(); // 删除当前对象指定的文件
 
-        bool exists(); // 测试当前 File 是否存在
+        bool exists() const; // 测试当前 File 是否存在
 
-        std::string getAbsolutePath(); // 返回由该对象表示的文件的绝对路径名
+        std::string getAbsolutePath() const; // 返回由该对象表示的文件的绝对路径名
         
-        std::string getName(); // 返回表示当前对象的文件名或路径名（如果是路径，则返回最后一级子路径名）
+        std::string getName() const; // 返回表示当前对象的文件名或路径名（如果是路径，则返回最后一级子路径名）
         
-        std::string getParent(); // 返回当前 File 对象所对应目录（最后一级子目录）的父目录名
+        std::string getParent() const; // 返回当前 File 对象所对应目录（最后一级子目录）的父目录名
         
-        bool isAbsolute(); // 测试当前 File 对象表示的文件是否为一个绝对路径名。该方法消除了不同平台的差异，可以直接判断 file 对象是否为绝对路径。在 UNIX/Linux/BSD 等系统上，如果路径名开头是一条斜线/，则表明该 File 对象对应一个绝对路径；在 Windows 等系统上，如果路径开头是盘符，则说明它是一个绝对路径。
+        bool isAbsolute() const; // 测试当前 File 对象表示的文件是否为一个绝对路径名。该方法消除了不同平台的差异，可以直接判断 file 对象是否为绝对路径。在 UNIX/Linux/BSD 等系统上，如果路径名开头是一条斜线/，则表明该 File 对象对应一个绝对路径；在 Windows 等系统上，如果路径开头是盘符，则说明它是一个绝对路径。
         
-        bool isDir(); // 测试当前 File 对象表示的文件是否为一个路径
+        bool isDir() const; // 测试当前 File 对象表示的文件是否为一个路径
         
-        bool isFile(); // 测试当前 File 对象表示的文件是否为一个“普通”文件
+        bool isFile() const; // 测试当前 File 对象表示的文件是否为一个“普通”文件
 
-        long/*pg::type::timetamp_t*/ lastModified(); // 返回当前 File 对象表示的文件最后修改的时间
+        long/*pg::type::timetamp_t*/ lastModified() const; // 返回当前 File 对象表示的文件最后修改的时间
 
-        std::size_t size(); // 返回当前 File 对象表示的文件长度
+        std::size_t size() const; // 返回当前 File 对象表示的文件长度
         
-        pg::type::Group<std::string> list(); // 返回当前 File 对象指定的路径的文件列表
+        pg::type::Group<std::string> list() const; // 返回当前 File 对象指定的路径的文件列表
 
         template <typename _Condition>
-        pg::type::Group<std::string> list(_Condition & condition); // 返回当前 File 对象指定的目录中满足指定过滤器的文件列表
+        pg::type::Group<std::string> list(_Condition & condition) const; // 返回当前 File 对象指定的目录中满足指定过滤器的文件列表
 
         bool mkdir(); // parent must exists , 创建一个目录，它的路径名由当前 File 对象指定
 
@@ -73,9 +77,14 @@ namespace pg::base {
         std::shared_ptr<pg::base::FDWrapper> getFDWrapper(); // get FDWrapper, which can write/read to/from file
 
         std::shared_ptr<pg::base::FILEWrapper> getFILEWrapper(); // get FILEWrapper, which can write/read to/from file
+    private:
+        void initCheck();
+
 
     private:
-        Handle handle_ = { 0 };
+        std::string path_;
+        struct stat stat_ = { 0 };
+        bool exists_ = true;
     };
 
 } // namespace pg::base
---
+++ src/File.cpp
@@ -0,0 +1,143 @@
+#include <base/File.h>
+
+#include <errno.h>
+
+#include <cctype>
+
+#ifdef linux
+const char pg::base::File::PATH_SEPARATOR = '/';
+// #elif defined _WIN32
+// const char pg::base::File::PATH_SEPARATOR = '\\';
+#endif
+
+pg::base::File::File(const std::string & pathname) : path_(pathname) { // 通过将给定路径名字符串转换成抽象路径名来创建一个新 File 实例。
+    initCheck();
+}
+
+pg::base::File::File(const File & parent, const std::string & child) : path_(parent.path_ + child) { // 通过给定的父抽象路径名和子路径名字符串创建一个新的File实例。
+    initCheck();
+}
+
+pg::base::File::File(const std::string & parent, const std::string & child) : path_(parent + child) { // 根据 parent 路径名字符串和 child 路径名字符串创建一个新 File 实例。
+    initCheck();
+} 
+
+// public-member-functions
+bool pg::base::File::readable() const { // 测试应用程序是否能从指定的文件中进行读取
+    __uid_t uid = getuid();
+    __gid_t gid = getgid();
+    
+    if (gid == stat_.st_gid) {
+        if (uid == stat_.st_uid)
+            return stat_.st_mode & S_IRUSR;
+        return stat_.st_mode & S_IRGRP;
+    } else return stat_.st_mode & S_IROTH;
+}
+
+bool pg::base::File::writable() const { // 测试应用程序是否能写当前文件
+    __uid_t uid = getuid();
+    __gid_t gid = getgid();
+    
+    if (gid == stat_.st_gid) {
+        if (uid == stat_.st_uid)
+            return stat_.st_mode & S_IWUSR;
+        return stat_.st_mode & S_IWGRP;
+    } else return stat_.st_mode & S_IWOTH;
+}
+
+bool pg::base::File::remove() { // 删除当前对象指定的文件
+
+}
+
+bool pg::base::File::exists() const { // 测试当前 File 是否存在 
+    return exists_;
+}
+
+std::string pg::base::File::getAbsolutePath() const { // 返回由该对象表示的文件的绝对路径名
+    return isAbsolute() ? path_ : std::string(get_current_dir_name()).append(path_);
+}
+
+std::string pg::base::File::getName() const { // 返回表示当前对象的文件名或路径名（如果是路径，则返回最后一级子路径名）
+    std::string::size_type pos = path_.find_last_of(File::PATH_SEPARATOR);
+    if (pos == std::string::npos) return path_;
+
+    return std::string(path_.begin() + pos + 1, path_.end());
+}
+
+std::string pg::base::File::getParent() const { // 返回当前 File 对象所对应目录（最后一级子目录）的父目录名
+    int pos = getAbsolutePath().find_last_of(PATH_SEPARATOR);
+    if (pos == path_.size() - 1) return path_;
+
+    return std::string(path_.begin(), path_.begin() + pos);
+}
+
+bool pg::base::File::isAbsolute() const { // 测试当前 File 对象表示的文件是否为一个绝对路径名。该方法消除了不同平台的差异，可以直接判断 file 对象是否为绝对路径。在 UNIX/Linux/BSD 等系统上，如果路径名开头是一条斜线/，则表明该 File 对象对应一个绝对路径；在 Windows 等系统上，如果路径开头是盘符，则说明它是一个绝对路径。
+#ifdef linux
+    return !path_.empty() && path_[0] == PATH_SEPARATOR;
+// #elif defined _WIN32
+//     return !path_.empty() && std::isalpha(path[0]); 
+#endif
+}
+
+bool pg::base::File::isDir() const { // 测试当前 File 对象表示的文件是否为一个路径
+    return S_ISDIR(stat_.st_mode);
+}
+
+bool pg::base::File::isFile() const { // 测试当前 File 对象表示的文件是否为一个“普通”文件
+    return !( S_ISDIR(stat_.st_mode) );
+}
+
+long/*pg::type::timetamp_t*/ pg::base::File::lastModified() const { // 返回当前 File 对象表示的文件最后修改的时间
+
+}
+
+std::size_t pg::base::File::size() const { // 返回当前 File 对象表示的文件长度
+    return stat_.st_size;
+}
+
+pg::type::Group<std::string> pg::base::File::list() const { // 返回当前 File 对象指定的路径的文件列表
+
+}
+
+bool pg::base::File::mkdir() { // parent must exists , 创建一个目录，它的路径名由当前 File 对象指定
+
+}
+
+bool pg::base::File::mkdirs() { // no request, 创建一个目录，它的路径名由当前 File 对象指定
+
+}
+
+bool pg::base::File::rename(const std::string &) { // 文件更名为给定name, no-path
+
+}
+
+bool pg::base::File::move(const std::string & path) {
+
+}
+
+bool pg::base::File::move(const pg::base::File & target) { 
+    // move the File to target-path, 
+    // if the target is a file, mv to its parent-path
+
+
+}
+
+// PG-Like-Desigh
+std::shared_ptr<pg::base::FDWrapper> pg::base::File::getFDWrapper() { // get FDWrapper, which can write/read to/from file
+
+} 
+
+std::shared_ptr<pg::base::FILEWrapper> pg::base::File::getFILEWrapper() { // get FILEWrapper, which can write/read to/from file
+
+}
+
+// private-functions
+void pg::base::File::initCheck() {
+    
+    // to check if path is valid
+    // ... using regex-match
+
+    if ( path_.size() > 1 && path_.back() == PATH_SEPARATOR ) path_.pop_back();
+    if (::stat(path_.c_str(), &stat_) == -1)
+        if (errno == ENOENT) exists_ = false;
+}
